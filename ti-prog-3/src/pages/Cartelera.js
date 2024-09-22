7c224267bb2e5b0d0cbc80dbc98153479918c123
Alejandro Villar <villars@udesa.edu.ar>
2024-09-22 00:47:49 -0300

    Populares y Cartelera de const a componentes

--- ti-prog-3/src/pages/Cartelera.js
+++ ti-prog-3/src/pages/Cartelera.js
@@ -1,14 +1,20 @@
 import Peliculas from "../components/Peliculas/Peliculas";
+import { Component } from "react";
 import { Link } from "react-router-dom";
 const cartelUrl = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=6d74e7317f9a497bee146a3eed86d6f7`;
 
-const Cartelera = () => {
-  return (
-    <>
-      <h2><Link to="/cartelera">Peliculas en cartelera</Link></h2>
-      <Peliculas url= {cartelUrl}/>
-    </>
-  )
+class Cartelera extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <>
+        <h2><Link to="/cartelera">Peliculas en cartelera</Link></h2>
+        <Peliculas url={cartelUrl} />
+      </>
+    )
+  }
 }
 
 export default Cartelera
--- ti-prog-3/src/pages/Populares.js
+++ ti-prog-3/src/pages/Populares.js
@@ -1,15 +1,24 @@
 import Peliculas from "../components/Peliculas/Peliculas";
+import { Component } from "react";
 import { Link } from "react-router-dom";
 //import "../components/Navbar/Navbar.css"
 const popularesUrl = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=6d74e7317f9a497bee146a3eed86d6f7`;
 
-const Populares = () => {
-  return (
-    <>
-      <h2 /* className="peliculasPopulares" */><Link to="/populares">Peliculas populares</Link></h2>
-      <Peliculas url= {popularesUrl}/>
-    </>
-  )
+class Populares extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <>
+      <h2 className="peliculasPopulares">
+        <Link to="/populares">Peliculas populares </Link>
+      </h2>
+      <Peliculas url={popularesUrl}></Peliculas>
+      </>
+    )
+}
 }
 
 export default Populares
+
