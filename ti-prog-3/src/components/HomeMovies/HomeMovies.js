ea2b5187217bc340b7518ed327e918e401ae8e5a
Oli Roldan <oroldan@udesa.edu.ar>
2024-09-23 14:49:20 -0300

    home component

---
+++ ti-prog-3/src/components/HomeMovies/HomeMovies.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import Pelicula from '../Pelicula/Pelicula';
+import { Link } from "react-router-dom";
+
+class HomeMovies extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      info: [],
+      verMas: false
+     };
+  }
+
+  componentDidMount() {
+    fetch(this.props.url)
+      .then(response => response.json())
+      .then(data => this.setState({ info: data.results }))
+      .catch(error => console.error(error));
+  }
+  handleVerMas(){
+    this.setState({
+      verMas: !this.state.verMas // muestra lo contrario de lo que ya tenia
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <section className='contenedor'>
+          <article>
+
+            <h3>{this.props.titulo}</h3>
+            
+            <div className='peli'>
+              {this.state.info.slice(0, 6).map((pelicula, index) => (<Pelicula key={index} pelicula={pelicula} />))}
+            </div>
+          </article>
+        </section>
+      </>
+    )
+  }
+
+}
+
+export default HomeMovies
--- ti-prog-3/src/pages/Home.js
+++ ti-prog-3/src/pages/Home.js
@@ -1,13 +1,14 @@
-import Populares from "./Populares";
-import Cartelera from "./Cartelera";
 import { Link } from "react-router-dom";
+import HomeMovies from "../components/HomeMovies/HomeMovies";
 
 const Home = () => {
     return (
         <>
-            <Populares/>
+            <Link to="/populares"><h2>Peliculas populares</h2></Link>
+            <HomeMovies url={`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=6d74e7317f9a497bee146a3eed86d6f7`}></HomeMovies>
             <Link to="/populares"><button>Ver todas</button></Link>
-            <Cartelera/>
+            <Link to= "/cartelera"><h2>Peliculas en cartelera</h2></Link> 
+            <HomeMovies url={`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=6d74e7317f9a497bee146a3eed86d6f7`}></HomeMovies>
             <Link to="/cartelera"><button>Ver todas</button></Link> 
         </>
     );
